6e6b923ffef02dad36a797edb7526f4df57df013
MrTrainCow <legoguy145@gmail.com>
2020-11-27 00:29:06 -0500

    fixed bug that prevented bot from not working

--- commands/search.js
+++ commands/search.js
@@ -2,6 +2,7 @@ const levenshtien = require("damerau-levenshtein");
 const itemArray = require("./../index_generator.js");
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 const doc = new GoogleSpreadsheet('18-pxaUaUvYxACE5uMveCE9_bewwhfbd93ZaLIyP_rxQ');
+const fs = require('fs');
 const queryOverrides = JSON.parse(fs.readFileSync('./../config/query_overrides.json', 'utf8'));
 
 const itemsObj = new itemArray(doc);
