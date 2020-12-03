0c0d6283fadd1e32330c678f3766476f13204229
MrTrainCow <legoguy145@gmail.com>
2020-12-02 23:46:44 -0500

    added help command

--- commands/about.js
+++ commands/about.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'about-me', // trigger phrase
 	description: 'States about me',
 	execute(message, args) {
-        let messageText = 'I am The Librarian. I was built to serve the patrons of this server in their quests for models. If you need me, use \`?_\` followed by your query, and I\'ll do my best to find them for you. If you find an error, contact my creator, MrTrainCow#5154.';
+        let messageText = 'I am The Librarian. I was built to serve the patrons of this server in their quest for models. If you need me, use \`?_\` followed by your query, and I\'ll do my best to find them for you. If you find an error, contact my creator, MrTrainCow#5154. For additional help, use \`?_help\`.';
         message.channel.send(messageText);
 		return messageText;
 	},
