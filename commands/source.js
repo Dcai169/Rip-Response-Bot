b5ade5c60aea547e1d3266be6181a2e7a0a7da7b
MrTrainCow <legoguy145@gmail.com>
2020-12-03 22:02:52 -0500

    added punctuation and removed old pfp

--- commands/source.js
+++ commands/source.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'source', // trigger phrase
 	description: 'States about me',
 	execute(message, args) {
-        let messageText = 'My source code can be found at <https://github.com/Dcai169/Rip-Response-Bot>';
+        let messageText = 'My source code can be found at <https://github.com/Dcai169/Rip-Response-Bot>.';
         message.channel.send(messageText);
 		return messageText;
 	},
