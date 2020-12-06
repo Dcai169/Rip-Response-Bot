72ad91cd222342ce0e1655554ef00891ae542a5d
MrTrainCow <legoguy145@gmail.com>
2020-12-05 23:50:01 -0500

    completed search() implementation

--- responders/HaloResponder.js
+++ responders/HaloResponder.js
@@ -41,7 +41,7 @@ class HaloResponder extends BaseResponder {
         // get new data
         this.doc.loadInfo().then(() => {
             this.doc.sheetsByIndex.forEach(sheet => {
-                sheet.loadCells().then(() => { 
+                sheet.loadCells().then(() => {
                     for (let row = 0; row < sheet.rowCount; row++) { // then add the data to the array
                         (async () => {
                             this.items[sheet.title].push(await this.createItemObj(sheet, row));
@@ -61,12 +61,52 @@ class HaloResponder extends BaseResponder {
 
     // SEARCHING
     search(_msg, query) {
-        let installment = undefined;
-
         // Detect what game the user wants (CE, 2A, Reach, etc)
-        installment = (() => {
-            if (query.split("halo ").length > 1) {
+        let gameToQuery = (() => {
+            let game = query.match(/^(h)?(alo)?(\s)?(\d)?([aceg:]{1,3})?((\s)?((anniversary)?(classic)?(guardians)?(odst)?(reach)?))?/gmi).shift().replace(/^(h)?(alo)?/gmi, '').toLowerCase();
+            if (game) {
+                if (/\d/.test(game)) { // if is h2, h2a, h3, h3 odst, h4, h5, hw2
+                    switch (game.match(/[2-5]/).shift()) { // match only numbers 2 through 5
+                        case '2':
+                            if (game.includes('wars')) {
+                                return 'Halo Wars 2';
+                            } else {
+                                if (game.includes('a')) {
+                                    return 'Halo 2 Anniversary';
+                                } else {
+                                    return 'Halo 2 Classic';
+                                }
+                            }
 
+                        case '3':
+                            if (game.includes('odst')) {
+                                return 'Halo 3: ODST';
+                            } else {
+                                return 'Halo 3';
+                            }
+
+                        case '4':
+                            return 'Halo 4';
+
+                        case '5':
+                            return 'Halo 5';
+
+                        default:
+                            break;
+                    }
+                } else { // else hce, hcea, reach, hw
+                    if (game.includes('reach')) {
+                        return 'Halo Reach';
+                    } else if (game.includes('wars')) {
+                        return 'Halo Wars';
+                    } else { // ce or cea
+                        if (game.includes('cea')) {
+                            return 'Halo CEA';
+                        } else {
+                            return 'Halo CE';
+                        }
+                    }
+                }
             } else {
                 return;
             }
@@ -80,6 +120,8 @@ class HaloResponder extends BaseResponder {
                 results = results.concat(BaseResponder.addParam(this.items[key].filter(this.itemFilter, query), 'game', key));
             }
         }
+
+        return results;
     }
 
     static generateQualifierString(game) {
