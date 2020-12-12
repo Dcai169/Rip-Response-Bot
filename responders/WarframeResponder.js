d998bf6bc0cfce5f492e36b1dd1f6478835904af
MrTrainCow <legoguy145@gmail.com>
2020-12-11 22:21:24 -0500

    disabled warframe sfm response

--- responders/WarframeResponder.js
+++ responders/WarframeResponder.js
@@ -75,14 +75,14 @@ class WarframeResponder extends BaseResponder {
 
     static resultResponse(result) {
         let response = `The ${this.generateFullyQualifiedName(result)} model is ${evaluateReplace(result.baseRip, { replacement: 'not available yet.', callback: (res) => { return `available at <${res}>.` } })}\n`;
-        let sfmPortCount = !!result.sfm1 + !!result.sfm2 + !!result.sfm3;
-        if (sfmPortCount === 1) {
-            response += `A SFM port is available at <${[result.sfm1, result.sfm2, result.sfm3].filter((res) => {return !!res}).join('')}>`;
-        } else if (sfmPortCount === 2) {
-            response += `SFM ports are available at ${[result.sfm1, result.sfm2, result.sfm3].filter((res) => {return !!res}).map((res) => {return `<${res}>`}).join(' and ')}`;
-        } else if (sfmPortCount === 3) {
-            response += `SFM ports are available at ${[result.sfm1, result.sfm2].map((res) => {return `<${res}>`}).join(', ')}, and <${result.sfm3}>.`
-        }
+        // let sfmPortCount = !!result.sfm1 + !!result.sfm2 + !!result.sfm3;
+        // if (sfmPortCount === 1) {
+        //     response += `A SFM port is available at <${[result.sfm1, result.sfm2, result.sfm3].filter((res) => {return !!res}).join('')}>`;
+        // } else if (sfmPortCount === 2) {
+        //     response += `SFM ports are available at ${[result.sfm1, result.sfm2, result.sfm3].filter((res) => {return !!res}).map((res) => {return `<${res}>`}).join(' and ')}`;
+        // } else if (sfmPortCount === 3) {
+        //     response += `SFM ports are available at ${[result.sfm1, result.sfm2].map((res) => {return `<${res}>`}).join(', ')}, and <${result.sfm3}>.`
+        // }
         
         return response;
     }
