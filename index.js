9056dca908a58bb531a7146cd5aeeca92a88d8da
MrTrainCow <legoguy145@gmail.com>
2020-12-12 12:42:48 -0500

    reenabled front page

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-// const express = require('express');
+const express = require('express');
 
-// Express Config
-// const web = express();
-// const PORT = process.env.PORT || 3000;
+Express Config
+const web = express();
+const PORT = process.env.PORT || 3000;
 
-// web.get('/', (req, res) => {
-//     res.send('Hello World!');
-// });
+web.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
-// web.get('/auth', (req, res) => {
-//     res.send('OAuth2 Flow Completed');
-// });
+web.get('/auth', (req, res) => {
+    res.send('OAuth2 Flow Completed');
+});
 
-// web.listen(PORT, () => {
-//     console.log(`Express running on port ${PORT}`);
-// });
+web.listen(PORT, () => {
+    console.log(`Express running on port ${PORT}`);
+});
