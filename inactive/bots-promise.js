2b9e6ed141094644cc0e8c8c20be76a16bfb947c
Daniel Cai <legoguy145@gmail.com>
2021-05-13 10:55:03 -0400

    cleanup: removed unneeded files

--- inactive/bots-promise.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'bots-promise', // trigger phrase
-	description: 'States the promise',
-	execute(message, args) {
-        message.channel.send('I will not tell lies.');
-		return 'I will not tell lies.';
-	},
-};
--- inactive/order-66.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'order-66', // trigger phrase
-	description: 'Executes order 66',
-	execute(message, args) {
-        message.channel.send('Yes my Lord.');
-		return 'Yes my Lord.';
-	},
-};
