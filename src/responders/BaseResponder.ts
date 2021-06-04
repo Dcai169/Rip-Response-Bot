18591d6d0920c8086948eb29510bd2b6335456ee
Daniel Cai <legoguy145@gmail.com>
2021-06-04 18:55:04 -0400

    fix: made filter less discriminant

--- src/responders/BaseResponder.ts
+++ src/responders/BaseResponder.ts
@@ -55,6 +55,6 @@ export abstract class BaseResponder {
     }
 
     itemFilter(this: string, entry: Entry): boolean {
-        return levenshtein((entry.name ? entry.name.toLowerCase().replace(/(\W)?$/gmi, '').replace(/\b((the\s)?((an?)\s)?(is)?){1}\b/gi, '') : ' '), this).similarity > parseFloat(process.env.SIMILARITY_THRESHOLD) // the Damerau-Levenshtien distance must greater than the specified number || entry.aliases.includes(this.toLowerCase()); // or if the query matches an alias
+        return levenshtein((entry.name ? entry.name.toLowerCase().replace(/(\W)?$/gmi, '').replace(/\b((the\s)?((an?)\s)?(is)?){1}\b/gi, '').replace(new RegExp('(set|suit)', 'gmi'), '').trim() : ' '), this).similarity > parseFloat(process.env.SIMILARITY_THRESHOLD) // the Damerau-Levenshtien distance must greater than the specified number || entry.aliases.includes(this.toLowerCase()); // or if the query matches an alias
     }
 }
