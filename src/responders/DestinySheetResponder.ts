bf771a8c0eb158afdd619c733f8a69b64e388ca7
Daniel Cai <legoguy145@gmail.com>
2021-07-14 00:58:23 -0400

    Update DestinySheetResponder.ts

--- src/responders/DestinySheetResponder.ts
+++ src/responders/DestinySheetResponder.ts
@@ -104,7 +104,7 @@ export class DestinySheetResponder extends SheetBaseResponder {
                 });
                 resolve();
             } catch (error) {
-                reject()
+                throw error;
             }
         });
     }
