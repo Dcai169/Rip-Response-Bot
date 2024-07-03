c19e319158825ecd583dc6222b4c11621f4abefb
Daniel Cai <17992507+Dcai169@users.noreply.github.com>
2024-07-03 11:59:26 -0400

    Update HaloSheetResponder.ts

--- src/responders/HaloSheetResponder.ts
+++ src/responders/HaloSheetResponder.ts
@@ -42,7 +42,8 @@ export class HaloSheetResponder extends SheetBaseResponder {
             ['Halo 5', []],
             ['Halo Wars', []],
             ['Halo Wars 2', []],
-            ['Halo: Spartan Strike', []]
+            ['Halo: Spartan Strike', []],
+            ['Halo Infinite', []]
         ]);
     }
 
